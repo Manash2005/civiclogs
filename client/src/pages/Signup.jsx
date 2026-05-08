ad622da164b19365b136091d40c74c23e4b43fea
Manash2005 <swainm099@gmail.com>
2026-05-09 00:48:44 +0530

    style : signup page setup successfull

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,14 +1,15 @@
 import {Link} from 'react-router-dom'
 import {useState} from 'react';
-import { Mail, Lock, Eye, EyeOff, ArrowRight } from 'lucide-react';
-function Login() {
-
+import {User, Mail, Lock, Eye, EyeOff, ArrowRight } from 'lucide-react';
+function Signup() {
   const[role, setRole] = useState('Citizen')
-  const [showPassword, setShowPassword] = useState(false);
-  return (
-  <div className="grid grid-cols-1 md:grid-cols-2 min-h-screen">
+  const [showPassword, setShowPassword] = useState(false);   
+  const[showConfirmPassword, setShowConfirmPassword] = useState(false);
 
-   {/* LEFT */}
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 min-h-screen">
+
+    {/* LEFT */}
     <div className="relative h-screen hidden md:block">
         {/* Image */}
         <img 
@@ -34,30 +35,28 @@ function Login() {
                 Connecting Citizens <br /> Building Communities
               </h2>
               <p className="text-lg text-gray-200 max-w-md">
-                Thank you for being a part of our community! Together, we can make a differenc.
+                Report issues, collaborate with volunteers,
+                and improve your community together.
               </p>
           </div>
         </div>
       </div>
+
   {/* RIGHT */}
   <div className="flex items-center justify-center bg-secondary">
     <div className="w-full max-w-md p-4">
       <div className='mb-6 py-4'>
-        <p className="text-5xl font-script font-bold mb-1 text-blackf">
-          Welcome Back
-        </p>
+        <h2 className="text-5xl font-script font-bold mb-1 text-black">
+          Create Your Account
+        </h2>
 
         <p className='text-sm text-gray-600'>
-          Please enter your details to sign in
+          Join civiclogs and be a part of the change
         </p>
       </div>
 
-      <p className="text-sm font-sans mb-2 text-gray-700">
-        SELECT ROLE
-      </p>
-
       <div className="flex bg-white rounded-xl p-1 border border-gray-400 mb-6">
-          {["Citizen", "Volunteer", "Admin"].map((item) => (
+          {["Citizen", "Volunteer"].map((item) => (
             <button
               key={item}
               onClick={() => setRole(item)}
@@ -73,6 +72,19 @@ function Login() {
         </div>
 
       <form>
+        <label className="block text-sm font-medium mb-2 text-neutral">
+          Full Name
+        </label>
+
+        <div className='flex items-center border border-gray-400 rounded-xl px-3 py-3 mb-5 bg-white'>
+          <User className="text-neutral mr-2" size={18} />
+          <input
+            type="text"
+            placeholder="Enter your full name"
+            className="w-full outline-none text-sm"
+          />
+        </div>
+
         <label className="block text-sm font-medium mb-2 text-neutral">
           Email Address
         </label>
@@ -91,9 +103,6 @@ function Login() {
           <label className="text-sm font-medium text-neutral">
             Password
           </label>
-          <button className="text-sm text-primary hover:underline">
-            Forgot Password?
-          </button>
         </div>
 
         <div className="flex items-center border border-gray-400 rounded-xl px-3 py-3 mb-6 bg-white">
@@ -118,6 +127,35 @@ function Login() {
             />
           )}
         </div>
+        {/* CONFIRM PASSWORD */}
+        <div className="flex justify-between items-center mb-2">
+          <label className="text-sm font-medium text-neutral">
+            Confirm Password
+          </label>
+        </div>
+
+        <div className="flex items-center border border-gray-400 rounded-xl px-3 py-3 mb-6 bg-white">
+          <Lock className="text-neutral mr-2" size={18} />
+          <input
+             type={showConfirmPassword ? "text" : "password"}
+            placeholder="••••••••"
+            className="w-full outline-none text-sm"
+          />
+          {/* ICON */}
+          {showConfirmPassword ? (
+            <Eye
+              className="ml-2 cursor-pointer text-neutral"
+              size={18}
+              onClick={() => setShowConfirmPassword(false)}
+            />
+          ) : (
+            <EyeOff
+              className="ml-2 cursor-pointer text-neutral"
+              size={18}
+              onClick={() => setShowConfirmPassword(true)}
+            />
+          )}
+        </div>
 
         {/* BUTTON */}
         <button className="w-full bg-primary text-white py-3 rounded-xl flex items-center justify-center gap-2 hover:bg-primary/90 transition-all duration-300">
@@ -125,10 +163,10 @@ function Login() {
         </button>
       </form>
       
-      <p className="text-sm text-gray-600 mt-4">
-        Don't have an account?{" "}
-        <Link to="/signup" className="text-primary font-medium hover:underline">
-          Sign Up
+      <p className="text-sm text-center text-gray-600 mt-4">
+        Already have an account?{" "}
+        <Link to="/" className="text-primary font-medium hover:underline">
+          Log In
         </Link>
       </p>
     </div>
@@ -138,4 +176,4 @@ function Login() {
   )
 }
 
-export default Login
+export default Signup
