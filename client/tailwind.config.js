8cef58bca8fc71e777f74a756e1bbf782e9939bd
Manash2005 <swainm099@gmail.com>
2026-05-08 12:18:25 +0530

    added tailwind config

---
+++ client/tailwind.config.js
@@ -0,0 +1,14 @@
+// tailwind.config.js
+export default {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#588157",
+        secondary: "#A3B18A",
+        tertiary: "#DAD7CD",
+        neutral: "#767873",
+        danger: "#DC2626",
+      },
+    },
+  },
+};
